3b89845f9bd8b05b6ab8086cb453cb20b27fa12b
Joey Musante <jrmusan@gmail.com>
2022-02-18 10:05:32 -0500

    Set up a sqlite db backend, and connected it to the front end via app.py and index.html

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 
+import sqlite3
 from flask import Flask, render_template
+
+# This will return a database connection to our SQLite db
+# This will return rows from the db that are just dicts
+def get_db_connection():
+	conn = sqlite3.connect('database.db')
+	conn.row_factory = sqlite3.Row
+	return conn
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	
+	# This willn open the db connection
+	conn = get_db_connection()
+	
+	# Next we select all entries in the (posts) table
+	posts = conn.execute('SELECT * FROM posts').fetchall()
+	conn.close()
+	return render_template('index.html', posts=posts)

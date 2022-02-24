827943a69148048b56ca2830709ca982df97703a
Joey Musante <jrmusan@gmail.com>
2022-02-23 21:01:50 -0500

    Added bit to route click of post to page to display it

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 import sqlite3
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 
 # This will return a database connection to our SQLite db
 # This will return rows from the db that are just dicts
@@ -10,8 +11,18 @@ def get_db_connection():
 	conn.row_factory = sqlite3.Row
 	return conn
 
+def get_post(post_id):
+	conn = get_db_connection()
+	post = conn.execute('SELECT * FROM posts WHERE id = ?',
+						(post_id,)).fetchone()
+	conn.close()
+	if post is None:
+		abort(404)
+	return post
+
 app = Flask(__name__)
 
+# This is just for the base home page route
 @app.route('/')
 def index():
 	
@@ -22,3 +33,10 @@ def index():
 	posts = conn.execute('SELECT * FROM posts').fetchall()
 	conn.close()
 	return render_template('index.html', posts=posts)
+
+
+# This is our route to see a post
+@app.route('/<int:post_id>')
+def post(post_id):
+	post = get_post(post_id)
+	return render_template('recipe.html', post=post)

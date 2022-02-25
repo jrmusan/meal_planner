701796ae25ab0a00214356b9304bec16b0037fc2
Joey Musante <jrmusan@gmail.com>
2022-02-24 20:21:37 -0500

    Added some helpful comments

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ def get_db_connection():
 
 def get_post(post_id):
 	conn = get_db_connection()
-	post = conn.execute('SELECT * FROM posts WHERE id = ?',
-						(post_id,)).fetchone()
+	
+	# This is running a sql command to get a posts by it's id
+	post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
 	conn.close()
 	if post is None:
 		abort(404)
@@ -23,26 +24,38 @@ def get_post(post_id):
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkeyohwowcrazy'
 
+"""
+================================================================================
+|																			   |
+|				    Below are the app routes which give us new we pages		   |
+|																			   |
+================================================================================
+"""
+
+
 # This is just for the base home page route
 @app.route('/')
 def index():
 	
-	# This willn open the db connection
+	# This will open the db connection
 	conn = get_db_connection()
 	
 	# Next we select all entries in the (posts) table
 	posts = conn.execute('SELECT * FROM posts').fetchall()
 	conn.close()
+	
+	# We render this page by passing in the posts we just returned from the db
 	return render_template('index.html', posts=posts)
 
 
-# This is our route to see a post
+#~~~~~~~~This is our route to see a post~~~~~~~~
 @app.route('/<int:post_id>')
 def post(post_id):
 	post = get_post(post_id)
 	return render_template('recipe.html', post=post)
 
-# This is our route to create a new post
+
+#~~~~~~~~This is our route to create a new post~~~~~~~~
 @app.route('/create', methods=('GET', 'POST'))
 def create():
 	
@@ -57,14 +70,14 @@ def create():
 		else:
 			# Lets write this to the database!
 			conn = get_db_connection()
-			conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-						(title, content))
+			conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',(title, content))
 			conn.commit()
 			conn.close()
 			return redirect(url_for('index'))
 	return render_template('create.html')
 
-# This guy will edit a recipe
+
+#~~~~~~~~This will edit a recipe~~~~~~~~
 @app.route('/<int:id>/edit', methods=('GET', 'POST'))
 def edit(id):
 	
@@ -79,9 +92,7 @@ def edit(id):
 			flash('Title is required!')
 		else:
 			conn = get_db_connection()
-			conn.execute('UPDATE posts SET title = ?, content = ?'
-						' WHERE id = ?',
-						(title, content, id))
+			conn.execute('UPDATE posts SET title = ?, content = ?'' WHERE id = ?', (title, content, id))
 			conn.commit()
 			conn.close()
 			return redirect(url_for('index'))

eb1cb4c30fabab637bab798e7f8bc39267734c65
Joey Musante <jrmusan@gmail.com>
2023-06-11 17:46:23 -0400

    Fixed bug where modal would only update on the first time Modify was pressed

--- static/js/internal/unit_scripts.js
+++ static/js/internal/unit_scripts.js
@@ -16,29 +16,28 @@ function getSelected() {
 
     // Doing this enables us to show/hide the modal and reserve its data
     // TODO: THIS WILL NOT ALLOW USERS TO UPDATE INGREDIENTS ONCE ADD SAVE HAS BEEN PRESSED
-    if( table ) {
-        $('#unitModal').modal('show');
-    } else {
-        
-        // Get the ingredients selected from the dropdown
-        const values = $('#ingredients').val();
-        console.log(values)
 
-        const option_list = '<option value="item">Item</option> <option value="cup">Cup</option> <option value="pound">Pound</option> <option value="ounce">Ounce</option> <option value="tbsp">Tbsp</option> <option value="tsp">Tsp</option>'
-        
-        // Build the table
-        table += '<table class="table"> <thead> <tr> <th scope="col">Ingredient</th> <th scope="col">Quantity</th> <th scope="col">Unit</th> </tr> </thead> <tbody>'
-        values.forEach(function(ing_id) {
-            ingredient = getIngredientById(ing_id);
-            table += '<tr class="ing-qty-row" data-ing-id="' + ingredient.id + '"> <td>' + ingredient.name + '</td>'
-            table += '<td>' + '<input type="number" value=1></td>'
-            table += '<td> <select id=' + ingredient.id + '_unit' + '>' + option_list + ' </select> </td>'
-            table += '</tr>'
-        }); 
-        table += '</tbody> </table>'
-        
-        // Send this value to the Modal
-        document.getElementById("ingredients_list").innerHTML = table
-    }
+    // Get the ingredients selected from the dropdown
+    const values = $('#ingredients').val();
+
+    console.log("Running getSelected(), and wiping modal")
+    var table = '';
+
+
+    const option_list = '<option value="item">Item</option> <option value="cup">Cup</option> <option value="pound">Pound</option> <option value="ounce">Ounce</option> <option value="tbsp">Tbsp</option> <option value="tsp">Tsp</option>'
+
+    // Build the table
+    table += '<table class="table"> <thead> <tr> <th scope="col">Ingredient</th> <th scope="col">Quantity</th> <th scope="col">Unit</th> </tr> </thead> <tbody>'
+    values.forEach(function(ing_id) {
+        ingredient = getIngredientById(ing_id);
+        table += '<tr class="ing-qty-row" data-ing-id="' + ingredient.id + '"> <td>' + ingredient.name + '</td>'
+        table += '<td>' + '<input type="number" value=1></td>'
+        table += '<td> <select id=' + ingredient.id + '_unit' + '>' + option_list + ' </select> </td>'
+        table += '</tr>'
+    });
+    table += '</tbody> </table>'
+
+    // Send this value to the Modal
+    document.getElementById("ingredients_list").innerHTML = table
 }
 

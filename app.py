92a131a883b4d2f1d89706fa5b2cf0c9e1563e4b
Joey Musante <jrmusan@gmail.com>
2024-03-30 22:02:18 -0400

    Still trying to fix adding an ingredient, removed possibly extra redirect after addition of ingredient

--- app.py
+++ app.py
@@ -169,7 +169,6 @@ def add_ingredient():
 			ing_obj = Ingredent(name.strip(), category=category)
 			ing_obj.insert_ingredient()
 			flash(f'Added: {name.strip()}')
-			return render_template('add_ingredient.html', ingredients=ingredients)
 	
 	return render_template('add_ingredient.html', ingredients=ingredients)
 		
